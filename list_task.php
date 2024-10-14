6925c84ce1b766b782503db7a0b2e06c2a5bf9ac
ahmedsaadx <ahmeddsaadd188@gmail.com>
2024-10-14 10:52:04 +0300

    add update feature

--- list_task.php
+++ list_task.php
@@ -12,6 +12,11 @@ $result = mysqli_query($conn, $query);
           <?php echo $_SESSION['success_delete_task']; ?>
         </div>
      <?php endif?>
+     <?php if(isset($_SESSION['update_task'])):?>
+        <div class="alert alert-success">
+          <?php echo $_SESSION['update_task']; ?>
+        </div>
+     <?php endif?>
   <div class="row">
     <?php $i = 1; while ($row = mysqli_fetch_array($result)) : ?>
       <div class="col-md-4 mb-4"> <!-- 3 cards per row for medium screens -->
@@ -21,8 +26,9 @@ $result = mysqli_query($conn, $query);
             <!-- <h5 class="card-title"><?php echo htmlspecialchars($row["name"]); ?></h5> -->
             <p class="card-text"><?php echo htmlspecialchars($row["description"]); ?></p>
             <p class="card-text"><small class="text-muted">Due Date: <?php echo htmlspecialchars($row["due_date"]); ?></small></p>
-            <a   class="btn btn-primary btn-sm">Edit</a>
+            <a  href="update_task.php?id=<?php echo $row['id']; ?>" class="btn btn-primary btn-sm">Edit</a>
             <a href="delete_task.php?id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm"  onclick="return confirm('Are you sure you want to delete this task?');"> Delete</a>
+            
           </div>
         </div>
       </div>
@@ -32,6 +38,9 @@ $result = mysqli_query($conn, $query);
 <?php
 if (isset($_SESSION['success_delete_task'])){
     unset($_SESSION['success_delete_task']);
+}
+if (isset($_SESSION['update_task'])){
+  unset($_SESSION['update_task']);
 }
 
 
